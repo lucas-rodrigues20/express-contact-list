4e32cdec4ace335ad3e91d2a4ca24b9e3d576630
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-04-24 16:04:04 -0300

    add feature for listing contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,7 +1,12 @@
 const models = require('../models');
 
 exports.get_contacts = function(req, res, next) {
-  res.render('contacts/contacts');
+
+  return models.Contact
+    .findAll()
+    .then(contacts => {
+      res.render('contacts/contacts', { contacts });
+    });
 };
 
 exports.submit_contact = function(req, res, next) {
