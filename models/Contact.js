8bd2fb0ef150dda5b54ffb6dabfef87617cc3949
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-04-24 15:39:18 -0300

    create Contact model and initialize sequelize in bin/www

---
+++ models/Contact.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  var Contact = sequelize.define('Contact', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return Contact;
+}
